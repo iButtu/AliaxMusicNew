654e563e8e337e362d00eeaa03f4fc76bf80e251
Buttu <129733301+iButtu@users.noreply.github.com>
2023-05-25 11:34:20 +0530

    Update __init__.py

--- AnonX/__init__.py
+++ AnonX/__init__.py
@@ -3,6 +3,7 @@ from AnonX.core.dir import dirr
 from AnonX.core.git import git
 from AnonX.core.userbot import Userbot
 from AnonX.misc import dbb, heroku, sudo
+from telethon import TelegramClient
 
 from .logging import LOGGER
 

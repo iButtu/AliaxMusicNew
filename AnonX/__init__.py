a2da5ee992afd313a60afe72b72dd63cac2e1f45
Buttu <129733301+iButtu@users.noreply.github.com>
2023-05-25 11:52:08 +0530

    Update __init__.py

--- AnonX/__init__.py
+++ AnonX/__init__.py
@@ -21,7 +21,13 @@ sudo()
 # Clients
 app = AnonXBot()
 userbot = Userbot()
-telethn = TelegramClient("AnonX")
+telethn = TelegramClient("AnonX", API_ID, API_HASH)
+
+else:
+    from AnonX.config import Development as Config
+
+    API_ID = Config.API_ID
+    API_HASH = Config.API_HASH
 
 
 from .platforms import *

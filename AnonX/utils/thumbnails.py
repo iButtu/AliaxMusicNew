a249e5e464958a5d2bb3392c4d3a9a4c8250815a
Vickyvky13 <131648662+Vickyvky13@users.noreply.github.com>
2023-11-29 10:14:58 +0530

    Add files via upload

--- AnonX/utils/thumbnails.py
+++ AnonX/utils/thumbnails.py
@@ -7,7 +7,7 @@ import aiohttp
 from PIL import (Image, ImageDraw, ImageEnhance, ImageFilter,
                  ImageFont, ImageOps)
 from youtubesearchpython.__future__ import VideosSearch
-from AnonX import app
+from FallenMusic import app
 from unidecode import unidecode
 from config import MUSIC_BOT_NAME, YOUTUBE_IMG_URL
 
